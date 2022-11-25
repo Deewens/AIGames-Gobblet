f11c87d9a48a21f4876b254175b1491ccc3ea26f
Adrien Dudon <c00278154@itcarlow.ie>
2022-11-25 13:41:36 +0000

    Add gobblet method with gobble up feature

---
+++ Gobblet/Gobblet/Gobblet.cpp
@@ -0,0 +1,48 @@
+﻿#include "Gobblet.hpp"
+
+Gobblet::Gobblet(const sf::Color& t_color, int t_size)
+    : m_color(t_color),
+      m_size(t_size)
+
+{
+    verifySize();
+}
+
+int Gobblet::getSize() const
+{
+    return m_size;
+}
+
+void Gobblet::verifySize()
+{
+    if (m_size > 4)
+    {
+        m_size = 4;
+    }
+    else if (m_size < 1)
+    {
+        m_size = 1;
+    }
+}
+
+bool Gobblet::gobbleUp(const std::shared_ptr<Gobblet>& t_biggerGobblet)
+{
+    if (t_biggerGobblet->m_size > m_size)
+    {
+        m_parentGobblet = t_biggerGobblet;
+        t_biggerGobblet->m_childGobblet = t_biggerGobblet;
+        return true;
+    }
+
+    return false;
+}
+
+std::shared_ptr<Gobblet> Gobblet::getParentGobblet()
+{
+    return m_parentGobblet;
+}
+
+std::shared_ptr<Gobblet> Gobblet::getGobbledUpGobblet()
+{
+    return m_childGobblet;
+}
---
+++ Gobblet/Gobblet/Gobblet.hpp
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include <memory>
+#include <stack>
+
+#include <SFML/Graphics/Color.hpp>
+
+class Gobblet
+{
+public:
+    /**
+     * \brief Create a new gobblet
+     * \param t_color Color of this gobblet (either black or white)
+     * \param t_size Gobblet size between 1 and 4
+     */
+    Gobblet(const sf::Color& t_color, int t_size);
+
+    int getSize() const;
+
+    /**
+     * \brief Gobble up this gobblet with a bigger one
+     * \param t_biggerGobblet Bigger gobblet
+     * \return true if the passed gobblet is bigger than this one, false otherwise
+     */
+    bool gobbleUp(const std::shared_ptr<Gobblet>& t_biggerGobblet);
+
+    std::shared_ptr<Gobblet> getParentGobblet();
+    std::shared_ptr<Gobblet> getGobbledUpGobblet();
+
+private:
+    
+    /**
+    * \brief Check if the size of the gobblet is between 1 and 4.
+    * \details If the size is more than 4, then the size will be set to 4. If the size lower than 1, then the size will be set to 1.
+    */
+    void verifySize();
+
+    sf::Color m_color;
+    int m_size;
+
+    std::shared_ptr<Gobblet> m_parentGobblet;
+    std::shared_ptr<Gobblet> m_childGobblet;
+};
