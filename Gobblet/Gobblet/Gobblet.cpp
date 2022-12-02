f6b9a6be6d49ed3d8a0c427b277c126ade8c5bfa
Adrien Dudon <c00278154@itcarlow.ie>
2022-12-02 16:15:07 +0000

    Adding drawable for Gobblet

--- Gobblet/Gobblet/Gobblet.cpp
+++ Gobblet/Gobblet/Gobblet.cpp
@@ -1,11 +1,24 @@
 ﻿#include "Gobblet.hpp"
 
-Gobblet::Gobblet(const sf::Color& t_color, int t_size)
-    : m_color(t_color),
-      m_size(t_size)
-
+Gobblet::Gobblet(const Color& t_color, const int t_size) :
+    m_size(t_size),
+    m_parentGobblet(nullptr),
+    m_childGobblet(nullptr),
+    m_shape(static_cast<float>(t_size))
 {
     verifySize();
+
+    if (t_color == Color::White)
+    {
+        m_color = sf::Color::White;
+    }
+    else
+    {
+        m_color = sf::Color::Black;
+    }
+    
+    m_shape.setOrigin(0.5f, 0.5f);
+    m_shape.setFillColor(m_color);
 }
 
 int Gobblet::getSize() const
@@ -13,24 +26,22 @@ int Gobblet::getSize() const
     return m_size;
 }
 
-void Gobblet::verifySize()
+sf::Vector2i Gobblet::getGridCoordinates() const
 {
-    if (m_size > 4)
-    {
-        m_size = 4;
-    }
-    else if (m_size < 1)
-    {
-        m_size = 1;
-    }
+    return *m_gridCoordinates;
 }
 
-bool Gobblet::gobbleUp(const std::shared_ptr<Gobblet>& t_biggerGobblet)
+void Gobblet::setGridCoordinates(const sf::Vector2i& coordinates)
 {
-    if (t_biggerGobblet->m_size > m_size)
+    m_gridCoordinates = std::make_unique<sf::Vector2i>(coordinates);
+}
+
+bool Gobblet::gobbleUp(Gobblet& t_biggerGobblet)
+{
+    if (t_biggerGobblet.m_size > m_size)
     {
-        m_parentGobblet = t_biggerGobblet;
-        t_biggerGobblet->m_childGobblet = t_biggerGobblet;
+        m_parentGobblet = std::make_shared<Gobblet>(t_biggerGobblet);
+        t_biggerGobblet.m_childGobblet = std::make_shared<Gobblet>(t_biggerGobblet);
         return true;
     }
 
@@ -45,4 +56,25 @@ std::shared_ptr<Gobblet> Gobblet::getParentGobblet()
 std::shared_ptr<Gobblet> Gobblet::getGobbledUpGobblet()
 {
     return m_childGobblet;
+}
+
+sf::Vector2f Gobblet::getPosition() const
+{
+    return m_shape.getPosition();
+}
+
+void Gobblet::verifySize()
+{
+    if (m_size > 4)
+    {
+        m_size = 4;
+    }
+    else if (m_size < 1)
+    {
+        m_size = 1;
+    }
+}
+
+void Gobblet::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
 }
--- Gobblet/Gobblet/Gobblet.hpp
+++ Gobblet/Gobblet/Gobblet.hpp
@@ -2,42 +2,55 @@
 
 #include <memory>
 #include <stack>
+#include <SFML/Graphics/CircleShape.hpp>
 
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Drawable.hpp>
 
-class Gobblet
+class Gobblet : sf::Drawable
 {
 public:
+    enum class Color { Black, White };
+    
     /**
      * \brief Create a new gobblet
      * \param t_color Color of this gobblet (either black or white)
      * \param t_size Gobblet size between 1 and 4
      */
-    Gobblet(const sf::Color& t_color, int t_size);
+    Gobblet(const Color& t_color, int t_size);
 
     int getSize() const;
+
+    sf::Vector2i getGridCoordinates() const;
+    void setGridCoordinates(const sf::Vector2i& coordinates);
 
     /**
      * \brief Gobble up this gobblet with a bigger one
      * \param t_biggerGobblet Bigger gobblet
      * \return true if the passed gobblet is bigger than this one, false otherwise
      */
-    bool gobbleUp(const std::shared_ptr<Gobblet>& t_biggerGobblet);
+    bool gobbleUp(Gobblet& t_biggerGobblet);
 
     std::shared_ptr<Gobblet> getParentGobblet();
     std::shared_ptr<Gobblet> getGobbledUpGobblet();
 
+    sf::Vector2f getPosition() const;
 private:
-    
     /**
     * \brief Check if the size of the gobblet is between 1 and 4.
     * \details If the size is more than 4, then the size will be set to 4. If the size lower than 1, then the size will be set to 1.
     */
     void verifySize();
 
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
     sf::Color m_color;
     int m_size;
 
-    std::shared_ptr<Gobblet> m_parentGobblet;
+    std::unique_ptr<sf::Vector2i> m_gridCoordinates;
+
+    Gobblet& m_parentGobblet;
     std::shared_ptr<Gobblet> m_childGobblet;
+
+    sf::CircleShape m_shape;
 };
